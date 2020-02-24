6a5fa40ace411996744c99321bd2cb080b6630e1
luca <luca.mcgyver@gmail.com>
2020-02-24 20:41:33 +0100

    differentiating master/branches pipelines, autoversioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,11 @@ plugins {
     id("maven-publish")
     id("com.jfrog.bintray") version "1.8.4"
     id("org.jetbrains.kotlin.js") version "1.3.61"
+    id("net.researchgate.release") version "2.6.0"
     id("com.github.node-gradle.node") version "2.2.1"
 }
 
-val cVersion = Version(0, 0, 1, true)
-
 group = "it.ldsoftware"
-version = cVersion.toString()
 
 repositories {
     mavenCentral()
@@ -45,25 +43,33 @@ tasks {
         key = System.getenv("BINTRAY_PASS")
         pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
             repo = "maven"
-            name = if (cVersion.snapshot) "${project.name}-branches" else project.name
+            name = if (isSnapshot()) "${project.name}-branches" else project.name
             setLicenses("MIT")
             vcsUrl = "https://github.com/LukeDS-it/firebase-ktyped"
             publish = true
+            version(delegateClosureOf<BintrayExtension.VersionConfig> {
+                name = if (isSnapshot()) snapshotVersion() else project.version.toString()
+            })
         })
         setPublications(project.name)
     }
 
+    release {
+        failOnSnapshotDependencies = false
+        newVersionCommitMessage = "[ci skip] automatic versioning"
+    }
 }
 
-data class Version(val major: Int, val minor: Int, val patch: Int, val snapshot: Boolean) {
-    override fun toString(): String {
-        val suffix = if (snapshot) {
-            "-" +
-                    (System.getenv("BRANCH") ?: "alpha") +
-                    "-" +
-                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))
-        } else ""
+gradle.taskGraph.whenReady {
+    if (isSnapshot())
+        version = snapshotVersion()
+}
 
-        return "$major.$minor.$patch$suffix"
-    }
+fun isSnapshot(): Boolean = (System.getenv("BRANCH") ?: "alpha") != "master"
+
+fun snapshotVersion(): String {
+    val current = """(\d*\.\d*\.\d*)""".toRegex().find(project.version.toString())!!.groups[0]!!.value
+    val branch = (System.getenv("BRANCH") ?: "alpha")
+    val date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))
+    return "$current-$branch-$date"
 }
